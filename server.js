4fd273da5ac95293de5acb212de44568b4291e3d
Stephen K. Davis <stephenkdavis@outlook.com>
2022-01-24 23:09:31 -0500

    Create Initial Project

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * * Smart Voting API
+ * * License: GNU General Public License v3.0
+ * * https://github.com/smartvoting
+ */
+
+const express = require("express");
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 8081;
