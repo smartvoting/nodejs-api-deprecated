80b8b91eb7479241062056d9d286b2950398deda
Stephen K. Davis <stephenkdavis@outlook.com>
2022-01-25 17:17:57 -0500

    Update page comment headers

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-/**
- * * Smart Voting API
- * * License: GNU General Public License v3.0
- * * https://github.com/smartvoting
- */
+/***************************************************************************************
+ *     _________                      __    ____   ____     __  .__                    *
+ *    /   _____/ _____ _____ ________/  |_  \   \ /   /____/  |_|__| ____    ____      *
+ *    \_____  \ /     \\__  \\_  __ \   __\  \   Y   /  _ \   __\  |/    \  / ___\     *
+ *    /        \  Y Y  \/ __ \|  | \/|  |     \     (  <_> )  | |  |   |  \/ /_/  >    *
+ *   /_______  /__|_|  (____  /__|   |__|      \___/ \____/|__| |__|___|  /\___  /     *
+ *           \/      \/     \/                                          \//_____/      *
+ ***************************************************************************************
+ *   Project Title: Smart Voting                                                       *
+ *   Project Website: https://smartvoting.cc/                                          *
+ *   Documentation: https://docs.smartvoting.cc/                                       *
+ *   Project Source Code: https://github.com/smartvoting/                              *
+ ***************************************************************************************
+ *   Project License: GNU General Public License v3.0                                  *
+ *   Project Authors: Matthew Campbell, Stephen Davis, Satabdi Sangma, Michael Sirna   *
+ *   George Brown College - Computer Programmer Analyst (T127)                         *
+ *   Capstone Project - Winter 2022 - COMP 3059 & COMP 3078                            *
+ ***************************************************************************************/
 
 require("dotenv").config();
 const queries = require("./sql/frequentCommands");
--- sql/tableList.js
+++ sql/tableList.js
@@ -1,3 +1,22 @@
+/***************************************************************************************
+ *     _________                      __    ____   ____     __  .__                    *
+ *    /   _____/ _____ _____ ________/  |_  \   \ /   /____/  |_|__| ____    ____      *
+ *    \_____  \ /     \\__  \\_  __ \   __\  \   Y   /  _ \   __\  |/    \  / ___\     *
+ *    /        \  Y Y  \/ __ \|  | \/|  |     \     (  <_> )  | |  |   |  \/ /_/  >    *
+ *   /_______  /__|_|  (____  /__|   |__|      \___/ \____/|__| |__|___|  /\___  /     *
+ *           \/      \/     \/                                          \//_____/      *
+ ***************************************************************************************
+ *   Project Title: Smart Voting                                                       *
+ *   Project Website: https://smartvoting.cc/                                          *
+ *   Documentation: https://docs.smartvoting.cc/                                       *
+ *   Project Source Code: https://github.com/smartvoting/                              *
+ ***************************************************************************************
+ *   Project License: GNU General Public License v3.0                                  *
+ *   Project Authors: Matthew Campbell, Stephen Davis, Satabdi Sangma, Michael Sirna   *
+ *   George Brown College - Computer Programmer Analyst (T127)                         *
+ *   Capstone Project - Winter 2022 - COMP 3059 & COMP 3078                            *
+ ***************************************************************************************/
+
 module.exports = {
   electionOfficials: 'public."electionOfficials"',
   partyContact: 'public."partyContact"',
@@ -7,5 +26,5 @@ module.exports = {
   ridingList: 'public."ridingList"',
   roleList: 'public."roleList"',
   voterList: 'public."voterList"',
-  voterSecurity: 'public."voterSecurity"'
+  voterSecurity: 'public."voterSecurity"',
 };
