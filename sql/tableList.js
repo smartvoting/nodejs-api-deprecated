6a2c513ae989fc7c0d9a97d65ef99b65e99e5e42
Stephen K. Davis <stephenkdavis@outlook.com>
2022-01-25 17:25:52 -0500

    Update header comments

--- sql/tableList.js
+++ sql/tableList.js
@@ -1,21 +1,21 @@
-/***************************************************************************************
- *     _________                      __    ____   ____     __  .__                    *
- *    /   _____/ _____ _____ ________/  |_  \   \ /   /____/  |_|__| ____    ____      *
- *    \_____  \ /     \\__  \\_  __ \   __\  \   Y   /  _ \   __\  |/    \  / ___\     *
- *    /        \  Y Y  \/ __ \|  | \/|  |     \     (  <_> )  | |  |   |  \/ /_/  >    *
- *   /_______  /__|_|  (____  /__|   |__|      \___/ \____/|__| |__|___|  /\___  /     *
- *           \/      \/     \/                                          \//_____/      *
- ***************************************************************************************
- *   Project Title: Smart Voting                                                       *
- *   Project Website: https://smartvoting.cc/                                          *
- *   Documentation: https://docs.smartvoting.cc/                                       *
- *   Project Source Code: https://github.com/smartvoting/                              *
- ***************************************************************************************
- *   Project License: GNU General Public License v3.0                                  *
- *   Project Authors: Matthew Campbell, Stephen Davis, Satabdi Sangma, Michael Sirna   *
- *   George Brown College - Computer Programmer Analyst (T127)                         *
- *   Capstone I & II Project - September 2021 to April 2022                            *
- ***************************************************************************************/
+/*****************************************************************************************
+ *     _________                      __       ____   ____     __  .__                   *
+ *    /   _____/ _____ _____ ________/  |_     \   \ /   /____/  |_|__| ____    ____     *
+ *    \_____  \ /     \\__  \\_  __ \   __\     \   Y   /  _ \   __\  |/    \  / ___\    *
+ *    /        \  Y Y  \/ __ \|  | \/|  |        \     (  <_> )  | |  |   |  \/ /_/  >   *
+ *   /_______  /__|_|  (____  /__|   |__|         \___/ \____/|__| |__|___|  /\___  /    *
+ *           \/      \/     \/                                             \//_____/     *
+ *****************************************************************************************
+ *   Project Title: Smart Voting                                                         *
+ *   Project Website: https://smartvoting.cc/                                            *
+ *   Documentation: https://docs.smartvoting.cc/                                         *
+ *   Project Source Code: https://github.com/smartvoting/                                *
+ *****************************************************************************************
+ *   Project License: GNU General Public License v3.0                                    *
+ *   Project Authors: Matthew Campbell, Stephen Davis, Satabdi Sangma, Michael Sirna     *
+ *   George Brown College - Computer Programmer Analyst (T127)                           *
+ *   Capstone I & II - September 2021 to April 2022                                      *
+ *****************************************************************************************/
 
 module.exports = {
   electionOfficials: 'public."electionOfficials"',
