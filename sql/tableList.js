ad9a5146748ec4950cf17244084abe8afcea4106
Stephen K. Davis <stephenkdavis@outlook.com>
2022-01-25 16:33:32 -0500

    PostgreSQL table list

---
+++ sql/tableList.js
@@ -0,0 +1,11 @@
+module.exports = {
+  electionOfficials: 'public."electionOfficials"',
+  partyContact: 'public."partyContact"',
+  partyList: 'public."partyList"',
+  partyStaff: 'public."partyStaff"',
+  platformTopics: 'public."platformTopics"',
+  ridingList: 'public."ridingList"',
+  roleList: 'public."roleList"',
+  voterList: 'public."voterList"',
+  voterSecurity: 'public."voterSecurity"'
+};
