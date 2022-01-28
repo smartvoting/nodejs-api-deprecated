d6bda0fb0aa85c219c7a56fe9493de33f3208534
Stephen K. Davis <stephenkdavis@outlook.com>
2022-01-27 21:23:32 -0500

    Add database manager

---
+++ sql/databases.js
@@ -0,0 +1,54 @@
+// const queries = require("./sql/frequentCommands");
+// const tables = require("./sql/tableList");
+// const pg = require("pg");
+
+// const pgClient = new pg.Client({
+//   connectionString: pgURL,
+// });
+
+// pgClient.connect();
+// app.get("/", function (req, res, next) {
+//   pgClient.query(
+//     queries.selectAll + tables.partyList,
+//     function (error, result) {
+//       if (error) {
+//         console.log(error);
+//       }
+//       res.status(200).send(result.rows);
+//     }
+//   );
+// });
+
+const _dynamoCS = "DYNAMO_CS";
+const _dynamoQueries = require("./dynamo/queries");
+const _dynamoSchemas = require("./dynamo/schemas");
+const _dynamoTables = require("./dynamo/tables");
+
+const _pgCS = `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
+const _pgQueries = require("./postgres/queries");
+const _pgTables = require("./postgres/tables");
+
+const _qldbCS = "QLDB_CS";
+const _qldbQueries = require("./qldb/queries");
+const _qldbSchemas = require("./qldb/schemas");
+const _qldbLedgers = require("./qldb/ledgers");
+
+module.exports = {
+  dynamo: {
+    cs: _dynamoCS,
+    queries: _dynamoQueries,
+    schemas: _dynamoSchemas,
+    tables: _dynamoTables,
+  },
+  postgres: {
+    cs: _pgCS,
+    queries: _pgQueries,
+    tables: _pgTables,
+  },
+  qldb: {
+    cs: _qldbCS,
+    ledgers: _qldbLedgers,
+    schemas: _qldbSchemas,
+    queries: _qldbQueries,
+  },
+};
